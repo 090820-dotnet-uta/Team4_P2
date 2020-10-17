370d44cf20e2f2a9afa3a5f1247e707f879c037d
Earl <acearl57@gmail.com>
2020-10-16 21:01:17 -0400

    added strings and fleshed out models

--- Team4_P2.Models/Models/Assignment.cs
+++ Team4_P2.Models/Models/Assignment.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Team4_P2.P2_Models
+namespace Team4_P2.Models
 {
     public class Assignment
     {
---
+++ Team4_P2.Repo/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Team4_P2.Models;
+namespace Team4_P2.Repo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Class> Classes { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
