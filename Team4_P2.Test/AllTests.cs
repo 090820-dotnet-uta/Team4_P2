74670d09513288dc062c481321974a4ac4851878
Earl <acearl57@gmail.com>
2020-10-21 16:57:08 -0400

    more tests

--- Team4_P2.Test/AllTests.cs
+++ Team4_P2.Test/AllTests.cs
@@ -11,72 +11,72 @@ namespace Team4_P2.Test
 {
     public class AllTests
     {
-          [Fact]
-          public async void AddAdminToDb()
-          {
-              var options = new DbContextOptionsBuilder<AppDbContext>()
-                  .UseInMemoryDatabase(databaseName: "yeet").Options;
-              using (var context = new AppDbContext(options))
-              {
-                  Repository repo = new Repository(context);
-                  Admin Admin = new Admin();
+        [Fact]
+        public async void AddAdminToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Admin Admin = new Admin();
 
-                  Admin = await repo.AddAdmin(Admin);
+                Admin = await repo.AddAdmin(Admin);
 
-                  Assert.True(context.Admins.Contains(Admin));
-              }
-          }
-          [Fact]
-          public async void DeleteAdminToDb()
-          {
-              var options = new DbContextOptionsBuilder<AppDbContext>()
-                  .UseInMemoryDatabase(databaseName: "yeet").Options;
-              using (var context = new AppDbContext(options))
-              {
-                  Repository repo = new Repository(context);
-                  Admin Admin = new Admin();
+                Assert.True(context.Admins.Contains(Admin));
+            }
+        }
+        [Fact]
+        public async void DeleteAdminToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Admin Admin = new Admin();
 
-                  Admin = await repo.AddAdmin(Admin);
+                Admin = await repo.AddAdmin(Admin);
 
-                  bool rip = await repo.DeleteAdmin(Admin);
-                  Assert.False(context.Admins.Contains(Admin));
-              }
-          }
-          [Fact]
-          public async void AddAssignmentToDb()
-          {
-              var options = new DbContextOptionsBuilder<AppDbContext>()
-                  .UseInMemoryDatabase(databaseName: "yeet").Options;
-              using (var context = new AppDbContext(options))
-              {
-                  Repository repo = new Repository(context);
-                  Assignment assignment = new Assignment();
-                  assignment.Title = "Ok boomer";
-                  assignment.EnrollmentId = 1;
+                bool rip = await repo.DeleteAdmin(Admin);
+                Assert.False(context.Admins.Contains(Admin));
+            }
+        }
+        [Fact]
+        public async void AddAssignmentToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Assignment assignment = new Assignment();
+                assignment.Title = "Ok boomer";
+                assignment.EnrollmentId = 1;
 
-                  assignment = await repo.AddAssignment(assignment);
+                assignment = await repo.AddAssignment(assignment);
 
-                  Assert.True(context.Assignments.Contains(assignment));
-              }
-          }
-          [Fact]
-          public async void DeleteAssignmentToDb()
-          {
-              var options = new DbContextOptionsBuilder<AppDbContext>()
-                  .UseInMemoryDatabase(databaseName: "yeet").Options;
-              using (var context = new AppDbContext(options))
-              {
-                  Repository repo = new Repository(context);
-                  Assignment Assignment = new Assignment();
-                  Assignment.Title = "Ok boomer";
-                  Assignment.EnrollmentId = 1;
+                Assert.True(context.Assignments.Contains(assignment));
+            }
+        }
+        [Fact]
+        public async void DeleteAssignmentToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Assignment Assignment = new Assignment();
+                Assignment.Title = "Ok boomer";
+                Assignment.EnrollmentId = 1;
 
-                  Assignment = await repo.AddAssignment(Assignment);
+                Assignment = await repo.AddAssignment(Assignment);
 
-                  bool rip = await repo.DeleteAssignment(Assignment.AssignmentId);
-                  Assert.False(context.Assignments.Contains(Assignment));
-              }
-          }
+                bool rip = await repo.DeleteAssignment(Assignment.AssignmentId);
+                Assert.False(context.Assignments.Contains(Assignment));
+            }
+        }
         [Fact]
         public async void AddClassToDb()
         {
@@ -139,6 +139,130 @@ namespace Team4_P2.Test
 
                 bool rip = await repo.DeleteCourse(course.CourseId);
                 Assert.False(context.Courses.Contains(course));
+            }
+        }
+        [Fact]
+        public async void AddEnrollmentToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Enrollment Enrollment = new Enrollment();
+
+                Enrollment = await repo.AddEnrollment(Enrollment);
+
+                Assert.True(context.Enrollments.Contains(Enrollment));
+            }
+        }
+        [Fact]
+        public async void DeleteEnrollmentToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Enrollment Enrollment = new Enrollment();
+
+                Enrollment = await repo.AddEnrollment(Enrollment);
+
+                bool rip = await repo.DeleteEnrollment(Enrollment.EnrollmentId);
+                Assert.False(context.Enrollments.Contains(Enrollment));
+            }
+        }
+        [Fact]
+        public async void AddStudentToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Student Student = new Student();
+
+                Student = await repo.AddStudent(Student);
+
+                Assert.True(context.Students.Contains(Student));
+            }
+        }
+        [Fact]
+        public async void DeleteStudentToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Student Student = new Student();
+
+                Student = await repo.AddStudent(Student);
+
+                bool rip = await repo.DeleteStudent(Student.StudentId);
+                Assert.False(context.Students.Contains(Student));
+            }
+        }
+        [Fact]
+        public async void AddTeacherToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Teacher Teacher = new Teacher();
+
+                Teacher = await repo.AddTeacher(Teacher);
+
+                Assert.True(context.Teachers.Contains(Teacher));
+            }
+        }
+        [Fact]
+        public async void DeleteTeacherToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                Teacher Teacher = new Teacher();
+
+                Teacher = await repo.AddTeacher(Teacher);
+
+                bool rip = await repo.DeleteTeacher(Teacher.TeacherId);
+                Assert.False(context.Teachers.Contains(Teacher));
+            }
+        }
+        [Fact]
+        public async void AddUserToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                User User = new User();
+
+                User = await repo.AddUser(User);
+
+                Assert.True(context.Users.Contains(User));
+            }
+        }
+        [Fact]
+        public async void DeleteUserToDb()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "yeet").Options;
+            using (var context = new AppDbContext(options))
+            {
+                Repository repo = new Repository(context);
+                User User = new User();
+
+                User = await repo.AddUser(User);
+
+                bool rip = await repo.DeleteUser(User.UserId);
+                Assert.False(context.Users.Contains(User));
             }
         }
     }
